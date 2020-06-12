8e6f066b3bb01ec1580740a03144e57b25530fce
Ryan Boggs <rmboggs@gmail.com>
2020-06-12 06:13:47 -0700

    Fix license copyright year.

--- src/Serialize.OpenXml.CodeGen/NamespaceAliasOrder.cs
+++ src/Serialize.OpenXml.CodeGen/NamespaceAliasOrder.cs
@@ -1,6 +1,6 @@
 /* MIT License
 
-Copyright (c) 2018 Ryan Boggs
+Copyright (c) 2020 Ryan Boggs
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this
 software and associated documentation files (the "Software"), to deal in the Software
